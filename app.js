0e6f58d46dc211577a62afda95d06953ace9d427
pascal <me@pascalchristian.com>
2020-06-30 04:33:36 +0000

    Redo indexing

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const season = '2019-20';
 
 /*
 GW player data 
-Use id as key
+Indexed by id
 */
 function gameweek_data(gw, season) {
 
@@ -18,16 +18,16 @@ function gameweek_data(gw, season) {
     request.get(endpoint, function (error, response, body) {
         if (!error && response.statusCode == 200) {
             const data = csv2json(body, {parseNumbers: true, hash: false});
-            const json = {};
+            const json = [];
             data.forEach(function(val, index, arr) {
-                let key = val.name.split('_')[2];
-                json[key] = {
-                    'id': parseInt(key),
-                    'first_name': player_data[key].first_name,
-                    'second_name': player_data[key].second_name,
-                    'web_name': player_data[key].web_name,
-                    'position': player_data[key].position,
-                    'team': player_data[key].team,
+                const player = player_data.filter(f => f.id === val.element)[0];
+                json[val.element] = {
+                    'id': val.element,
+                    'first_name': player.first_name,
+                    'second_name': player.second_name,
+                    'web_name': player.web_name,
+                    'position': player.position,
+                    'team': player.team,
                     ...val
                     };
             });
@@ -44,27 +44,24 @@ function gameweek_data(gw, season) {
 
 /*
 Player list 
-Use id as key
+Indexed by id
 */
 function player_list(season) {
 
     const endpoint1 = vaastav_fpl + season + '/player_idlist.csv';
     const endpoint2 = vaastav_fpl + season + '/players_raw.csv';
     const save_location = 'data/' + season + '/players.json';
-    const gw1_data = JSON.parse(fs.readFileSync('data/' + season + '/gws/gw1.json'));
     
     request.get(endpoint1, function (error, response, body) {
         if (!error && response.statusCode == 200) {
-            const json1 = csv2json(body, {parseNumbers: true, hash: false});
+            const data1 = csv2json(body, {parseNumbers: true, hash: false});
             request.get(endpoint2, function (error, response, body) {
                 if (!error && response.statusCode == 200) {
-                    const json2 = csv2json(body, {parseNumbers: true, hash: false});
-                    const data = Object. assign(json1, json2);
-                    // Reformat 
-                    const json = {};
+                    const data2 = csv2json(body, {parseNumbers: true, hash: false});
+                    const data = Object.assign(data1, data2);
+                    const json = [];
                     data.forEach(function(val, index, arr) {
-                        let key = val.id;
-                        json[key] = {
+                        json[val.id] = {
                             'id': val.id,
                             'first_name': val.first_name,
                             'second_name': val.second_name,
@@ -74,8 +71,7 @@ function player_list(season) {
                             'news': val.news,
                             'news_added': val.news_added,
                             'key': `${val.first_name}_${val.second_name}_${val.id}`,
-                            'starting_price': gw1_data[key] && gw1_data[key].value || 0,
-                        };
+                       };
                     });
                     fs.writeFile(save_location, JSON.stringify(json), function (err) {
                         if (err) throw err;
@@ -95,7 +91,7 @@ function player_list(season) {
 
 /*
 Team list 
-Use id as key (1 to 20)
+Indexed by id (1 to 20)
 */
 function team_list(season) {
 
@@ -105,11 +101,9 @@ function team_list(season) {
     request.get(endpoint, function (error, response, body) {
         if (!error && response.statusCode == 200) {
             const data = csv2json(body, {parseNumbers: true, hash: false});
-            // Reformat with id as key
-            const json = {};
+            const json = [];
             data.forEach(function(val, index, arr) {
-                let key = val.id;
-                json[key] = val;
+                json[val.id] = val;
             });
             fs.writeFile(save_location, JSON.stringify(json), function (err) {
                 if (err) throw err;
@@ -124,26 +118,24 @@ function team_list(season) {
 
 /*
 Fixtures list 
-Use id as key
+Indexed by id
 */
-function fixtures_list(gw, season) {
+function fixtures_list(season) {
 
     const endpoint = vaastav_fpl + season + '/fixtures.csv';
-    const save_location = 'data/' + season + `/fixtures/gw${gw}.json`;
+    const save_location = 'data/' + season + `/fixtures.json`;
     
     request.get(endpoint, function (error, response, body) {
         if (!error && response.statusCode == 200) {
             const data = csv2json(body, {parseNumbers: true, hash: false});
-            // Only return data for specified gameweek
-            const json = {};
-            data.filter((val) => val.event === gw).forEach(function(val, index, arr) {
-                let key = val.id;
-                json[key] = val;
-                json[key]['stats'] = JSON.parse(val.stats.replace(/'/g,'"'));
+            const json = [];
+            data.forEach(function(val, index, arr) {
+                json[val.id] = val;
+                json[val.id]['stats'] = JSON.parse(val.stats.replace(/'/g,'"'));
             });
             fs.writeFile(save_location, JSON.stringify(json), function (err) {
                 if (err) throw err;
-                console.log(`GW${gw} fixtures saved at ${save_location}` );
+                console.log(`Fixtures saved at ${save_location}` );
               });            
         } else {
             console.log('Error!');
@@ -157,7 +149,7 @@ function fixtures_list(gw, season) {
 // For GW1 to GW40
 for (i = 1; i <= 40; i++) {
     gameweek_data(i, season);
-    fixtures_list(i, season);
 }
 player_list(season);
 team_list(season);
+fixtures_list(season);
